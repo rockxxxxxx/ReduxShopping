f898a4d47d09d139bbbe96198d89112b62580396
rockxxxxxx <78143717+rockxxxxxx@users.noreply.github.com>
2022-11-17 00:39:09 +0530

    Doing network calls

--- src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import cartSlice from "../components/Cart/cartSlice";
-
-export const store = configureStore({
-  reducer: {
-    cartToggler: cartSlice,
-  },
-});
